1c337bc6460d485f2216ab40214b0863347f43de
Tim Hazlett <timhazlett2016@gmail.com>
2026-07-11 17:54:40 -0500

    WIP - Adding the other rules but need to check these

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { eslintCompatPlugin } from '@oxlint/plugins';
 
+import { optionsFactoryShape } from '#src/rules/options-factory-shape';
 import { optionsFileLocation } from '#src/rules/options-file-location';
+import { queryOptionsRequireKeyFactory } from '#src/rules/query-options-require-key-factory';
+import { requireQueryOptions } from '#src/rules/require-query-options';
 
 export const thazLintRules = eslintCompatPlugin({
   meta: {
     name: 'thaz-collective-standards',
   },
   rules: {
+    'options-factory-shape': optionsFactoryShape,
     'options-file-location': optionsFileLocation,
+    'query-options-require-key-factory': queryOptionsRequireKeyFactory,
+    'require-query-options': requireQueryOptions,
   },
 });
 
